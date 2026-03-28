f017a3508ae27551ee5d1a580ae09d530c5a6340
AdamPostMontjoie <57641108+AdamPostMontjoie@users.noreply.github.com>
2026-03-28 08:38:26 -0400

    scanner basic

--- FoodSizer/Features/Scanner/ScannerPageFeature.swift
+++ FoodSizer/Features/Scanner/ScannerPageFeature.swift
@@ -16,28 +16,24 @@ struct ScannerPageFeature {
     }
     
     var body: some ReducerOf<Self> {
-        // Connect the Camera child to this parent
         Scope(state: \.camera, action: \.camera) {
             CameraFeature()
         }
         
         Reduce { state, action in
             switch action {
-            // Intercept the camera button tap and trigger the navigation     
             case .camera:
                 return .none
-                
             case .path:
                 return .none
             }
         }
-        // Connect the Path enum
         .forEach(\.path, action: \.path)
     }
     
-    // Notice Camera is removed from here. Only pushed screens go in the Path.
     @Reducer(state: .equatable)
     enum Path {
         case scanReview(ScanReviewFeature)
     }
 }
+
